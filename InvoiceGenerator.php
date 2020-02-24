276a18a95f6ca49a1f1df91a2f787f08621773a3
Aminul Islam Mamun <mamun.amin1994@gmail.com>
2020-02-24 16:05:23 +0600

    First Commit

---
+++ InvoiceGenerator.php
@@ -0,0 +1,35 @@
+<?php
+	
+class InvoiceGenerator
+{
+	public function generate(Array $customerInfo, Array $shopInfo, Array $productInfo)
+	{
+		$template = file_get_contents(__DIR__ . '/base-template.html');
+		foreach($customerInfo as $property => $value) {
+			$template = str_replace($property, $value, $template);
+		}
+
+		foreach($shopInfo as $property => $value) {
+			$template = str_replace($property, $value, $template);
+		}
+
+		$total = 0;
+
+		$product_template = '';
+		foreach($productInfo as $singleProduct) {
+			$product = file_get_contents(__DIR__ . '/product-template.html');
+			foreach($singleProduct as $property => $value) {
+				$product = str_replace($property, $value, $product);
+			}
+			$total += $singleProduct['PRODUCT_INFO_RATE'];
+			$product_template .= $product;
+		}
+
+
+
+		$template = str_replace('PRODUCT_SECTION', $product_template, $template);
+		$template = str_replace('INVOICE_INFO_TOTAL', $total, $template);
+		return $template;
+	}
+}
+
---
+++ call.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+
+
+include 'InvoiceGenerator.php';
+
+
+$customerInfo = array(
+	"CUSTOMER_INFO_NAME" => "Sheikh Hasina",
+	"CUSTOMER_INFO_ADDRESS" => "Bijoy Sarani",
+	"CUSTOMER_INFO_PHONE" => "01313019149"
+);
+
+$shopInfo = array(
+	"SHOP_INFO_NAME" => "Alauddin STORE",
+	"SHOP_INFO_PHONE" => "0176262621",
+	"SHOP_INFO_ADDRESS" => "121, New Market, Dhaka"
+);
+
+$productInfo = array(
+	[
+		"PRODUCT_INFO_RATE" => "390",
+		"PRODUCT_INFO_EXPIRE" => "November 19, 2021",
+		"PRODUCT_INFO_SIZE" => "350 ml"
+	],
+	[
+		"PRODUCT_INFO_RATE" => "390",
+		"PRODUCT_INFO_EXPIRE" => "November 19, 2021",
+		"PRODUCT_INFO_SIZE" => "350 ml"
+	]
+);
+
+$test = new InvoiceGenerator();
+
+$html =  $test->generate($customerInfo, $shopInfo, $productInfo);
+
+$dompdf = new Dompdf();
+$dompdf->loadHtml($html);
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+$dompdf->stream();
